0f4474b4385fd0cc8007d8a3fa950dbbe8b6d8ca
mannanshihab <amannanshihab@gmail.com>
2024-12-02 19:40:25 +0600

    add:HomeCards

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
   import Navber from './components/Navber.vue';
   import Hero from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
 </script>
 <template>
   <Navber />
   <Hero />
+  <HomeCards />
 </template>
 
 
