6cb48e4c472b2514637e6a329cf7ccfabc847501
mannanshihab <amannanshihab@gmail.com>
2024-12-04 11:16:06 +0600

    update

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -15,7 +15,7 @@
                     Browse our Vue jobs and start your career today
                 </p>
                 <a
-                    href="jobs.html"
+                    href="#"
                     class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
                 >
                     Browse Jobs
@@ -28,7 +28,7 @@
                     List your job to find the perfect developer for the role
                 </p>
                 <a
-                    href="add-job.html"
+                    href="#"
                     class="inline-block bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-600"
                 >
                     Add Job
--- src/components/Navber.vue
+++ src/components/Navber.vue
@@ -24,12 +24,12 @@
                   >Home</a
                 >
                 <a
-                  href="jobs.html"
+                  href="#"
                   class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
                   >Jobs</a
                 >
                 <a
-                  href="add-job.html"
+                  href="#"
                   class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
                   >Add Job</a
                 >
