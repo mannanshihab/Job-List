019aadd11be4dcff364e0c13c18c21f84625ba54
mannanshihab <amannanshihab@gmail.com>
2024-12-04 10:03:48 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   import Navber from './components/Navber.vue';
   import Hero from './components/Hero.vue';
   import HomeCards from './components/HomeCards.vue';
+  import JobListing from './components/JobListing.vue';
 </script>
 <template>
   <Navber />
   <Hero />
   <HomeCards />
+  <JobListing />
 </template>
-
 
---
+++ src/components/JobListing.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import jobData from '@/jobs.json';
+    import { ref } from 'vue';
+
+    const jobs = ref(jobData);
+</script>
+
+<template>
+    <h1>Job Listing</h1>
+    <div v-for="job in jobs" :key="job.id">
+        <h2>{{abc }}</h2>
+    </div>
+</template>
